80ed51164906ba364ae31f0d85e045e265c5e0e9
Arun Syriac <arun.syriac@outlook.com>
2021-07-13 13:36:31 +0530

    update title

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
 import postReducer from "../features/post/postSlice";
 import profileReducer from "../features/profile/profileSlice";
@@ -7,7 +6,6 @@ import searchReducer from "../features/search/searchSlice";
 
 export const store = configureStore({
   reducer: {
-    counter2: counterReducer,
     auth: authReducer,
     post: postReducer,
     profile: profileReducer,
